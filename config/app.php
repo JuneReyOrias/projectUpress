f2a1b6f42b2077d7cd473f2b81c85d22621c9d5e
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2024-03-14 11:50:25 +0800

    Testing ap

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Barryvdh\DomPDF\ServiceProvider::class,
+       
     ])->toArray(),
 
     /*
@@ -184,7 +184,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'PDF'=> Barryvdh\DomPDF\Facade::class,
+      
     ])->toArray(),
 
 ];
